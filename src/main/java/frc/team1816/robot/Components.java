47029f7fc25ba8e2be572a49654e6ac8c1e4e691
Luke <luke.langefels@comcast.net>
2018-01-19 20:13:51 -0600

    added collector memes

--- src/main/java/frc/team1816/robot/Components.java
+++ src/main/java/frc/team1816/robot/Components.java
@@ -1,12 +1,14 @@
 package frc.team1816.robot;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import frc.team1816.robot.subsystems.Collector;
 import frc.team1816.robot.subsystems.Drivetrain;
 
 public class Components {
     private static Components instance;
 
     public Drivetrain drivetrain;
+    public Collector collector;
     public AnalogInput ai;
 
     private static final int RIGHT_MAIN = 4;
@@ -15,11 +17,14 @@ public class Components {
     private static final int LEFT_MAIN = 1;
     private static final int LEFT_SLAVE_ONE = 2;
     private static final int LEFT_SLAVE_TWO = 3;
+    private static final int COLLECTOR_LEFT = 7;
+    private static final int COLLECTOR_RIGHT = 8;
 
     private static final int PORT_CHANNEL = 0;
 
     public Components(){
         drivetrain = new Drivetrain(RIGHT_MAIN, RIGHT_SLAVE_ONE, RIGHT_SLAVE_TWO, LEFT_MAIN, LEFT_SLAVE_ONE, LEFT_SLAVE_TWO);
+        collector = new Collector(COLLECTOR_RIGHT, COLLECTOR_LEFT);
         ai = new AnalogInput(PORT_CHANNEL);
     }
 
