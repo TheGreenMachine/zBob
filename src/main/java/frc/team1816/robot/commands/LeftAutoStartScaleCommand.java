096029aa1cc22cf34ceda52d8af2d0408fd5440f
Andrew Hou <andrewfhou@gmail.com>
2018-02-19 12:28:56 -0600

    2-cube same-side switch auto

--- src/main/java/frc/team1816/robot/commands/LeftAutoStartScaleCommand.java
+++ src/main/java/frc/team1816/robot/commands/LeftAutoStartScaleCommand.java
@@ -25,24 +25,36 @@ public class LeftAutoStartScaleCommand extends CommandGroup {
         if (target == 'L') {
             System.out.println("Scale ---- Target: L");
 
-            //Experimental 45-turn Auto
-
-//            addParallel(new SetElevatorHeightPercentCommand(60));
             addParallel(new RaiseElevatorCommand());
             addSequential(new DriveXInchesCommand(285,0.8));
-//            addSequential(new RaiseElevatorCommand());
 
             addSequential(new WaitCommand(0.1));
-            addSequential(new RotateXDegreesCommand(45,true,0.5),2);
+            addSequential(new RotateXDegreesCommand(45,true,0.5),3);
             addSequential(new WaitCommand(0.1));
 
             addSequential(new ToggleCollectorCommand(true));
             addSequential(new SetCollectorSpeedCommand(-1));
             addSequential(new WaitCommand(1));
-            addSequential(new DriveXInchesCommand(4,-0.6),5);
-            addSequential(new LowerElevatorCommand());
+            addSequential(new DriveXInchesCommand(13,-0.6),5);
+            addParallel(new LowerElevatorCommand());
             addSequential(new SetCollectorSpeedCommand(0));
             addSequential(new WaitCommand(0.2));
+
+            addSequential(new RotateXDegreesCommand(114,true,0.5),3);
+            addParallel(new SetCollectorSpeedCommand(1));
+            addSequential(new DriveXInchesCommand(68,0.6,0.4,0.1));
+            addSequential(new WaitCommand(0.5));
+
+            addSequential(new DriveXInchesCommand(68,-0.8,0.6,0.2));
+            addParallel(new RaiseElevatorCommand());
+            addSequential(new RotateXDegreesCommand(-114,true,0.4));
+            addSequential(new SetCollectorSpeedCommand(0));
+//            addSequential(new RaiseElevatorCommand());
+            addSequential(new DriveXInchesCommand(12,0.8));
+            addSequential(new WaitCommand(1));
+            addSequential(new SetCollectorSpeedCommand(-1));
+            addSequential(new WaitCommand(0.5));
+            addSequential(new SetCollectorSpeedCommand(0));
 
         } else if (target == 'R') {
             System.out.println("Scale ---- Target: R");
