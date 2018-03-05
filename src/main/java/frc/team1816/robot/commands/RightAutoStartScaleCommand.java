4d53e573a42a4b8d9a291abdb89dadc2f5f9f3ac
mmmmunu <mnguron19@gmail.com>
2018-03-05 16:11:03 -0600

    Tuned RightAutoStartScaleCommand on non regulation carpet, now only the final rotateXDegrees and DriveX need to be changed.

--- src/main/java/frc/team1816/robot/commands/RightAutoStartScaleCommand.java
+++ src/main/java/frc/team1816/robot/commands/RightAutoStartScaleCommand.java
@@ -27,32 +27,33 @@ public class RightAutoStartScaleCommand extends CommandGroup {
             if(dsString.charAt(0) == 'R') {
 
                 System.out.println("2 Cube Scale/Switch ---- Target: R");
-
-                addParallel(new RaiseCollectorClawCommand(),1);
+                addSequential(new WaitCommand(0.1));
+//                addParallel(new RaiseCollectorClawCommand(),1);
 
                 //Cube One
                 addParallel(new RaiseElevatorCommand(1));
                 addSequential(new DriveXInchesCommand(285, 1)); //prev v 0.8
 
                 addSequential(new WaitCommand(0.1));
-                addSequential(new RotateXDegreesCommand(-45, true, 0.5), 3); //prev v 0.6
+                addSequential(new RotateXDegreesCommand(-45, true, 0.4), 3); //prev v 0.6
                 addSequential(new WaitCommand(.5)); //messing with
+                addSequential(new DriveXInchesCommand(28,1));
 
-                addSequential(new SetCollectorSpeedCommand(-1));
+                addSequential(new SetCollectorSpeedCommand(1));
                 addSequential(new WaitCommand(0.3));
-                addSequential(new DriveXInchesCommand(13, -0.8), 5); //prev v -0.6
+                addSequential(new DriveXInchesCommand(24, -0.8), 5); //prev v -0.6
                 addParallel(new LowerElevatorCommand());
                 addSequential(new SetCollectorSpeedCommand(0));
                 addSequential(new WaitCommand(0.5));
 
                 //Turn and collect second cube
-                addSequential(new RotateXDegreesCommand(-111, true, 0.5), 3); //prev v 0.6
+                addSequential(new RotateXDegreesCommand(-96, true, 0.4), 3); //prev v 0.6
                 addParallel(new SetCollectorSpeedCommand(1));
-                addSequential(new DriveXInchesCommand(80, 0.8, 0.4, 0.2), 4); //prev v 0.6
+                addSequential(new DriveXInchesCommand(84, 0.8, 0.4, 0.2), 4); //prev v 0.6
                 addSequential(new WaitCommand(0.3));
 
                 //Forwards and place second cube on switch
-                addSequential(new DriveXInchesCommand(12, -0.8, 0.6, 0.2),1);
+                addSequential(new DriveXInchesCommand(16, -0.8, 0.6, 0.2),1);
                 addParallel(new SetElevatorHeightPercentCommand(35),2);
                 addSequential(new SetCollectorSpeedCommand(0));
                 addSequential(new DriveXInchesCommand(4, 1),1); //prev v 0.8
@@ -65,7 +66,7 @@ public class RightAutoStartScaleCommand extends CommandGroup {
             } else {
                 System.out.println("2 Cube Scale ---- Target: L");
 
-                addParallel(new RaiseCollectorClawCommand(),1);
+//                addParallel(new RaiseCollectorClawCommand(),1);
 
                 //Cube One
                 addParallel(new RaiseElevatorCommand(2));
@@ -106,7 +107,7 @@ public class RightAutoStartScaleCommand extends CommandGroup {
             addSequential(new DriveXInchesCommand(240, 0.7,0.4,0.2));
             addSequential(new RotateXDegreesCommand(90,true,.4),3);
             addParallel(new RaiseElevatorCommand());
-            addSequential(new DriveXInchesCommand(56,0.8),5);
+            addSequential(new DriveXInchesCommand(52,0.8),5);
 
             addSequential(new WaitCommand(1));
             addSequential(new SetCollectorSpeedCommand(-1));
