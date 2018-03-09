8a585b71719d0c99e130cecc7e1fffd863fc9378
Andrew Hou <andrewfhou@gmail.com>
2018-03-09 16:06:06 -0600

    automatic-auto tweaks, needs to be mirrored to other autos

--- src/main/java/frc/team1816/robot/commands/LeftAutoStartCommand.java
+++ src/main/java/frc/team1816/robot/commands/LeftAutoStartCommand.java
@@ -27,7 +27,7 @@ public class LeftAutoStartCommand extends CommandGroup {
             addParallel(new LowerCollectorClawCommand(false,3));
             addParallel(new SetElevatorHeightPercentCommand(40,.5),2);
 
-            addSequential(new DriveXInchesCommand(128, 0.8));
+            addSequential(new DriveXInchesCommand(134, 0.8));
             addSequential(new RotateXDegreesCommand(90, true,.3),3);
             addSequential(new DriveXInchesCommand(4, 0.5),3);
 
@@ -48,8 +48,8 @@ public class LeftAutoStartCommand extends CommandGroup {
             addParallel(new LowerCollectorClawCommand(false,3));
 
             //Cube One
-            addParallel(new RaiseElevatorCommand(2));
-            addSequential(new DriveXInchesCommand(270, 0.7));
+            addParallel(new RaiseElevatorCommand(1));
+            addSequential(new DriveXInchesCommand(270, 0.6));
             addSequential(new WaitCommand(0.1));
             addSequential(new RotateXDegreesCommand(45, true, 0.3), 3);
             addSequential(new WaitCommand(0.1));
@@ -62,7 +62,7 @@ public class LeftAutoStartCommand extends CommandGroup {
             addSequential(new WaitCommand(0.2));
 
             //Turn and collect second cube
-            addSequential(new RotateXDegreesCommand(110, true, 0.3), 3);
+            addSequential(new RotateXDegreesCommand(115, true, 0.3), 3);
             addParallel(new SetCollectorSpeedCommand(-1));
             addSequential(new DriveXInchesCommand(72, 0.7, 0.4, 0.2), 4);
             addSequential(new WaitCommand(0.5));
