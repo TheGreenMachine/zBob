db1bf4b6cd05d1881465353f2ad2553c8e2302fb
PDeutsch <pete.deutsch@gmail.com>
2018-10-15 10:04:10 -0500

    Added test code to PPTest so show calc results

--- src/main/java/com/edinarobotics/purepursuit/PPTest.java
+++ src/main/java/com/edinarobotics/purepursuit/PPTest.java
@@ -1,7 +1,34 @@
 package com.edinarobotics.purepursuit;
 
 public class PPTest {
+    /**
+     * Run this by executing:  java com.edinarobotics.purpursuit.PPTest xRobot yRobot numPts maxAngle.
+     *
+     * For example this command will show 21 points going from -20 degrees to 20 degrees for a robot at 0, 0:
+     *
+     *     java com.edinarobotics.purepursuit.PPTest 0.0 0.0 21 20
+     */
     public static void main(String [] args) {
-        //todo: test code
+        PPPoint pt1 = new PPPoint(0.0, 0.0);
+        PPPoint pt2 = new PPPoint(0.0, 100.0);
+        double lookAheadDist = 10.0;
+        double targetVelocity = 0.5;
+
+        PurePursuitCalc calc = new PurePursuitCalc(pt1, pt2, lookAheadDist, targetVelocity);
+        double heading;
+        double x = (args.length > 0) ? Double.parseDouble(args[0]) : 0.0;
+        double y = (args.length > 1) ? Double.parseDouble(args[1]) : 0.0;
+        final int numPoints = (args.length > 2) ? Integer.parseInt(args[2]) : 11;
+        double maxAngle = (args.length > 3) ? Double.parseDouble(args[3]) : 20;
+        double stepSize = maxAngle * 2.0 / ((double)numPoints - 1.0);
+
+        for (int i = 0; i < numPoints; i++) {
+            heading = -maxAngle + ((double)i * stepSize);
+            double[] data = calc.calcVelocities(x, y, heading);
+            double[] alldata = calc.getData();
+            String msg = String.format("currX=%6.2f, currY=%6.2f, currHeading=%6.2f, desiredHeading=%6.2f, leftSetV=%6.2f, rightSetV=%6.2f",
+                    alldata[0], alldata[1], alldata[2], alldata[3], alldata[4], alldata[5]);
+            System.out.println(msg);
+        }
     }
 }
