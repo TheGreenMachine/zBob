8fc67e41438f36c7eb118b0dceb0038acc3b313b
Andrew Hou <andrewfhou@gmail.com>
2018-11-26 19:07:42 -0600

    added test endpoint command to PurePursuitAutoTestCommand

--- src/main/java/frc/team1816/robot/commands/PurePursuitAutoTestCommand.java
+++ src/main/java/frc/team1816/robot/commands/PurePursuitAutoTestCommand.java
@@ -8,12 +8,12 @@ public class PurePursuitAutoTestCommand extends CommandGroup {
     private PPPoint pt1 = new PPPoint(0,0);
     private PPPoint pt2 = new PPPoint(0, 24);
     private PPPoint pt3 = new PPPoint(36, 36);
-//    private PPPoint pt4 = new PPPoint(60, 60);
+   private PPPoint pt4 = new PPPoint(60, 60);
 
     public PurePursuitAutoTestCommand() {
         addSequential(new PurePursuitLineCommand(pt1 ,pt2, 4, 0.25));
         addSequential(new PurePursuitLineCommand(pt2, pt3, 4, 0.25));
-//        addSequential(new PurePursuitLineCommand(pt3, pt4, 12, 0.25));
+        addSequential(new PurePursuitEndpointCommand(pt3, pt4, 4, 0.25));
 
         System.out.println("Test Command End");
     }
